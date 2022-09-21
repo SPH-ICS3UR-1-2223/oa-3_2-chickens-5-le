3d140f9c65911296a69e1bfbc4810d00c958b782
TL <113382633+5-le@users.noreply.github.com>
2022-09-21 14:33:00 -0400

    trial 2

--- Chickens/src/Chickens01.java
+++ Chickens/src/Chickens01.java
@@ -2,12 +2,15 @@ public class Chickens01 {
     public static void main(String[] args) {
         //Put your code here
         
-        int eggsPerChicken = 5;
-        int chickenCount = 3;
+        int eggsPerChicken = 4;
+        int chickenCount = 8;
         int totalEggs = 0 ;
+        //Monday
         totalEggs += chickenCount * eggsPerChicken;
+        //Tuesday
         chickenCount = chickenCount + 1;
         totalEggs += chickenCount * eggsPerChicken;
+        //Wednesday
         chickenCount = chickenCount / 2;
         totalEggs += chickenCount * eggsPerChicken;
         
