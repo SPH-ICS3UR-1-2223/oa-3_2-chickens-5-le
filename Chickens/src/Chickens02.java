dd8ae717d19e95348aa43e289c616546341800f9
TL <113382633+5-le@users.noreply.github.com>
2022-09-23 13:50:08 -0400

    Chickens02

--- Chickens/src/Chickens02.java
+++ Chickens/src/Chickens02.java
@@ -2,10 +2,27 @@ public class Chickens02 {
     public static void main(String[] args) {
         //Put your code here
 
+         double dailyAverage = 0;
+         double eggCount = 0;
+         double monthlyAverage = 0;
+         double monthlyProfit = 0;
+         //Monday
+         eggCount+=100; //Collect 100 eggs
+         //Tuesday
+         eggCount+=121; //Collect 121 eggs
+         //Wednesday
+        eggCount+=117; //Collect 1177 eggs
         
-        
+        //Calculate Daily Average
+        dailyAverage = eggCount/3;
         System.out.println("Daily Average:   " +dailyAverage);
+        
+        //Calculate the Monthly Average
+        monthlyAverage = dailyAverage*30;
         System.out.println("Monthly Average: " +monthlyAverage);
+        
+        //Monthly Profit
+        monthlyProfit = monthlyAverage*0.18;
         System.out.println("Monthly Profit:  $" +monthlyProfit);
     }
     
